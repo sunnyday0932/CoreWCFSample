578f52db628818c555b4980dc6af8154714f517f
sunnday0932 <suunyday0932>
2023-07-15 11:26:04 +0800

    [Feat] WebHttp binding and using swagger document

--- src/Server/CoreWCFServer/CoreWCFServer/Program.cs
+++ src/Server/CoreWCFServer/CoreWCFServer/Program.cs
@@ -1,44 +1,61 @@
+using System.Net;
 using CoreWCF;
 using CoreWCF.Channels;
 using CoreWCF.Configuration;
 using CoreWCF.Description;
 using CoreWCFServer.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 const string HOST_IN_WSDL = "localhost";
 const int HTTP_PORT = 8088;
 const int HTTPS_PORT = 8443;
 const int NETTCP_PORT = 8089;
+const int WEPHTTP_PORT = 8080;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseNetTcp(NETTCP_PORT);
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(HTTP_PORT);
-    options.ListenAnyIP(HTTPS_PORT, listenOptions => { listenOptions.UseHttps(); });
+    // options.ListenAnyIP(HTTP_PORT);
+    // options.ListenAnyIP(HTTPS_PORT, listenOptions => { listenOptions.UseHttps(); });
+    options.AllowSynchronousIO = true;
+    options.ListenLocalhost(WEPHTTP_PORT);
 });
 
 // Add WSDL support
 builder.Services.AddServiceModelServices().AddServiceModelMetadata();
+builder.Services.AddServiceModelWebServices();
 // Use the scheme/host/port used to fetch WSDL as that service endpoint address in generated WSDL 
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+//UseSwaggerDocument
+builder.Services.AddSingleton(new SwaggerOptions());
 
 var app = builder.Build();
+app.UseMiddleware<SwaggerMiddleware>();
+app.UseSwaggerUI();
 
 app.UseServiceModel(builder =>
 {
-    builder.AddService<EchoService>((serviceOptions) =>
+    builder.AddService<EchoService>()
+        .AddServiceWebEndpoint<EchoService, IEchoService>("api", behavior =>
         {
-            // Set the default host name:port in generated WSDL and the base path for the address 
-            serviceOptions.BaseAddresses.Add(new Uri($"http://{HOST_IN_WSDL}/EchoService"));
-            serviceOptions.BaseAddresses.Add(new Uri($"https://{HOST_IN_WSDL}/EchoService"));
-        })
-        .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/basicHttp")
-        .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/basichttp")
-        // Add WSHttpBinding endpoints
-        .AddServiceEndpoint<EchoService, IEchoService>(new WSHttpBinding(SecurityMode.None), "/wsHttp")
-
-        // Add NetTcpBinding
-        .AddServiceEndpoint<EchoService,
-            IEchoService>(new NetTcpBinding(), $"net.tcp://localhost:{NETTCP_PORT}/EchoService/netTcp");
+            behavior.HelpEnabled = true;
+            behavior.AutomaticFormatSelectionEnabled = true;
+        } );
+    // builder.AddService<EchoService>((serviceOptions) =>
+    //     {
+    //         // Set the default host name:port in generated WSDL and the base path for the address 
+    //         serviceOptions.BaseAddresses.Add(new Uri($"http://{HOST_IN_WSDL}/EchoService"));
+    //         serviceOptions.BaseAddresses.Add(new Uri($"https://{HOST_IN_WSDL}/EchoService"));
+    //     })
+    //     .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/basicHttp")
+    //     .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport),
+    //         "/basichttp")
+    //     // Add WSHttpBinding endpoints
+    //     .AddServiceEndpoint<EchoService, IEchoService>(new WSHttpBinding(SecurityMode.None), "/wsHttp")
+    //
+    //     // Add NetTcpBinding
+    //     .AddServiceEndpoint<EchoService,
+    //         IEchoService>(new NetTcpBinding(), $"net.tcp://localhost:{NETTCP_PORT}/EchoService/netTcp");
 });
 
 // Enable WSDL for http & https
--- src/Server/CoreWCFServer/CoreWCFServer/Services/IEchoService.cs
+++ src/Server/CoreWCFServer/CoreWCFServer/Services/IEchoService.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Runtime.Serialization;
 using CoreWCF;
+using CoreWCF.OpenApi.Attributes;
+using CoreWCF.Web;
 
 namespace CoreWCFServer.Services;
 
@@ -19,21 +22,27 @@ public class EchoFault
 }
 
 [ServiceContract]
+[OpenApiBasePath("/api")]
 public interface IEchoService
 {
     [OperationContract]
+    [WebGet(UriTemplate = "/hello/{text}")]
+    [OpenApiResponse(ContentTypes = new[] { "application/json"}, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(string))]
     string Echo(string text);
 
     [OperationContract]
-    string ComplexEcho(EchoMessage text);
+    [WebInvoke(Method = "POST", UriTemplate = "/echo")]
+    [OpenApiResponse(ContentTypes = new[] { "application/json"}, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(EchoMessage))]
+    string ComplexEcho(
+        [OpenApiParameter(ContentTypes = new[] { "application/json"}, Description = "Text")] EchoMessage text);
 
     [OperationContract]
     [FaultContract(typeof(EchoFault))]
     string FailEcho(string text);
 }
 
-[DataContract]
+[DataContract(Name = "EchoMessage")]
 public class EchoMessage
 {
-    [AllowNull] [DataMember] public string Text { get; set; }
+    [AllowNull] [DataMember][OpenApiProperty(Description = "Text")] public string Text { get; set; }
 }
