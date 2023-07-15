0d379a91698a51b1a54bca7d5d67548fe56a05bf
sunnday0932 <suunyday0932>
2023-07-15 10:20:04 +0800

    [Feat] WCF Binding with NetTcp And Http

--- src/Client/CoreWCFClient/CoreWCFClient/Program.cs
+++ src/Client/CoreWCFClient/CoreWCFClient/Program.cs
@@ -1,3 +1,92 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.ServiceModel;
+using CoreWCFServer.Services;
+
+const int HTTP_PORT = 8088;
+const int HTTPS_PORT = 8443;
+const int NETTCP_PORT = 8089;
+
+
+Console.Title = "WCF .Net Core Client";
+
+CallBasicHttpBinding($"http://localhost:{HTTP_PORT}");
+CallBasicHttpBinding($"https://localhost:{HTTPS_PORT}");
+CallWsHttpBinding($"http://localhost:{HTTP_PORT}");
+CallNetTcpBinding($"net.tcp://localhost:{NETTCP_PORT}");
+
+
+static void CallBasicHttpBinding(string hostAddr)
+{
+    IClientChannel channel = null;
+
+    var binding =
+        new BasicHttpBinding(IsHttps(hostAddr) ? BasicHttpSecurityMode.Transport : BasicHttpSecurityMode.None);
+
+    var factory = new ChannelFactory<IEchoService>(binding, new EndpointAddress($"{hostAddr}/EchoService/basicHttp"));
+    factory.Open();
+    try
+    {
+        IEchoService client = factory.CreateChannel();
+        channel = client as IClientChannel;
+        channel.Open();
+        var result = client.Echo("Hello World!");
+        channel.Close();
+        Console.WriteLine(result);
+    }
+    finally
+    {
+        factory.Close();
+    }
+}
+
+static void CallWsHttpBinding(string hostAddr)
+{
+    IClientChannel channel = null;
+
+    var binding = new WSHttpBinding(IsHttps(hostAddr) ? SecurityMode.Transport : SecurityMode.None);
+
+    var factory = new ChannelFactory<IEchoService>(binding, new EndpointAddress($"{hostAddr}/EchoService/wsHttp"));
+    factory.Open();
+    try
+    {
+        IEchoService client = factory.CreateChannel();
+        channel = client as IClientChannel;
+        channel.Open();
+        var result = client.Echo("Hello World!");
+        channel.Close();
+        Console.WriteLine(result);
+    }
+    finally
+    {
+        factory.Close();
+    }
+}
+
+static void CallNetTcpBinding(string hostAddr)
+{
+    IClientChannel channel = null;
+
+    var binding = new NetTcpBinding();
+
+    var factory = new ChannelFactory<IEchoService>(binding, new EndpointAddress($"{hostAddr}/EchoService/netTcp"));
+    factory.Open();
+    try
+    {
+        IEchoService client = factory.CreateChannel();
+        channel = client as IClientChannel;
+        channel.Open();
+        var result = client.Echo("Hello World!");
+        channel.Close();
+        Console.WriteLine(result);
+    }
+    finally
+    {
+        factory.Close();
+    }
+}
+
+static bool IsHttps(string url)
+{
+    return url.ToLower().StartsWith("https://");
+}
--- src/Server/CoreWCFServer/CoreWCFServer/Program.cs
+++ src/Server/CoreWCFServer/CoreWCFServer/Program.cs
@@ -1,6 +1,49 @@
+using CoreWCF;
+using CoreWCF.Channels;
+using CoreWCF.Configuration;
+using CoreWCF.Description;
+using CoreWCFServer.Services;
+
+const string HOST_IN_WSDL = "localhost";
+const int HTTP_PORT = 8088;
+const int HTTPS_PORT = 8443;
+const int NETTCP_PORT = 8089;
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseNetTcp(NETTCP_PORT);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(HTTP_PORT);
+    options.ListenAnyIP(HTTPS_PORT, listenOptions => { listenOptions.UseHttps(); });
+});
+
+// Add WSDL support
+builder.Services.AddServiceModelServices().AddServiceModelMetadata();
+// Use the scheme/host/port used to fetch WSDL as that service endpoint address in generated WSDL 
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseServiceModel(builder =>
+{
+    builder.AddService<EchoService>((serviceOptions) =>
+        {
+            // Set the default host name:port in generated WSDL and the base path for the address 
+            serviceOptions.BaseAddresses.Add(new Uri($"http://{HOST_IN_WSDL}/EchoService"));
+            serviceOptions.BaseAddresses.Add(new Uri($"https://{HOST_IN_WSDL}/EchoService"));
+        })
+        .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/basicHttp")
+        .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/basichttp")
+        // Add WSHttpBinding endpoints
+        .AddServiceEndpoint<EchoService, IEchoService>(new WSHttpBinding(SecurityMode.None), "/wsHttp")
+
+        // Add NetTcpBinding
+        .AddServiceEndpoint<EchoService,
+            IEchoService>(new NetTcpBinding(), $"net.tcp://localhost:{NETTCP_PORT}/EchoService/netTcp");
+});
+
+// Enable WSDL for http & https
+var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+serviceMetadataBehavior.HttpGetEnabled = serviceMetadataBehavior.HttpsGetEnabled = true;
+
 
 app.Run();
